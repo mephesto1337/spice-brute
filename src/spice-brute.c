54bdbcea1fc60f8854aeb57f5103cab683fa44f4
Thomas WACHE <thomas@wache.fr>
2022-07-12 10:26:41 +0200

    Added getopt usage

--- src/spice-brute.c
+++ src/spice-brute.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <errno.h>
+#include <getopt.h>
 #include <glib.h>
 #include <spice-client.h>
 #include <spice/vd_agent.h>
@@ -12,7 +13,7 @@
 #include "spice-channel-types.h"
 #include "utils.h"
 
-#define THREADS_COUNT 16
+#define THREADS_COUNT 16LU
 
 GMainLoop *loop;
 typedef struct {
@@ -36,24 +37,102 @@ void try_next_password(SpiceSession *session, thread_args_t *args);
 size_t count_passwords_in_file(lock_file_t *f);
 int print_status_cb(gpointer user_data);
 void g_thread_func(gpointer data, gpointer user_data);
+void usage(void);
 
 int main(int argc, char *const argv[]) {
-    if (argc != 3) {
-        printf("usage: %s URI PASSWORD_FILE\n", program_invocation_short_name);
-        printf("  URI: spice://HOST?port=PORT\n");
+    GThreadPool *tp = NULL;
+    lock_file_t *password_file = NULL;
+    // Options
+    int opt;
+    const char *port = NULL;
+    const char *hostname = NULL;
+    const char *uri = NULL;
+    const char *wordlist = NULL;
+    size_t threads_count = THREADS_COUNT;
+    const struct option options[] = {{"threads", required_argument, NULL, 't'},
+                                     {"host", required_argument, NULL, 'H'},
+                                     {"port", required_argument, NULL, 'p'},
+                                     {"uri", required_argument, NULL, 'u'},
+                                     {"wordlist", required_argument, NULL, 'w'},
+                                     {"help", no_argument, NULL, 'h'},
+                                     {NULL, 0, NULL, 0}};
+
+    while ((opt = getopt_long(argc, argv, "hp:t:w:H:u:", options, NULL)) != EOF) {
+        switch (opt) {
+            case 'h':
+                usage();
+                return EXIT_SUCCESS;
+            case 'p': {
+                u_int16_t p;
+                if (sscanf(optarg, "%hu", &p) != 1) {
+                    usage();
+                    fprintf(stderr, "Invalid port value: '%s'\n", optarg);
+                    return EXIT_FAILURE;
+                }
+                port = optarg;
+                break;
+            }
+            case 't':
+                if (sscanf(optarg, "%lu", &threads_count) != 1) {
+                    usage();
+                    fprintf(stderr, "Invalid thread count value: '%s'\n", optarg);
+                    return EXIT_FAILURE;
+                }
+                break;
+            case 'w':
+                wordlist = optarg;
+                break;
+            case 'H':
+                hostname = optarg;
+                break;
+            case 'u':
+                if (uri != NULL) {
+                    free((void *)uri);
+                    uri = NULL;
+                }
+                CHK_NULL(uri = strdup(optarg));
+                break;
+            default:
+                usage();
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (wordlist == NULL) {
+        usage();
+        fprintf(stderr, "wordlist argument is mandatory\n");
         return EXIT_FAILURE;
     }
 
-    GThreadPool *tp = NULL;
-    lock_file_t *password_file = NULL;
-    const char *uri = argv[1];
+    if (uri == NULL) {
+        if (hostname == NULL) {
+            usage();
+            fprintf(stderr, "Either URI or HOSTNAME must be provided.\n");
+            return EXIT_FAILURE;
+        }
 
-    CHK_NULL(password_file = lock_file_open(argv[2], "r"));
+        char *new_uri = NULL;
+        if (port == NULL) {
+            CHK_NEG(asprintf(&new_uri, "spice://%s", hostname));
+        } else {
+            CHK_NEG(asprintf(&new_uri, "spice://%s?port=%s", hostname, port));
+        }
+        uri = new_uri;
+    } else {
+        if (hostname != NULL) {
+            usage();
+            fprintf(stderr, "Either one of URI or HOSTNAME must be provided.\n");
+            return EXIT_FAILURE;
+        }
+    }
+    assert(uri != NULL);
+
+    CHK_NULL(password_file = lock_file_open(wordlist, "r"));
     size_t total_password_count = count_passwords_in_file(password_file);
     printf("%lu passwords to try.", total_password_count);
 
     CHK_NULL(loop = g_main_loop_new(NULL, FALSE));
-    CHK_NULL(tp = g_thread_pool_new(g_thread_func, NULL, THREADS_COUNT, FALSE, NULL));
+    CHK_NULL(tp = g_thread_pool_new(g_thread_func, NULL, threads_count, FALSE, NULL));
 
     print_status_args_t ps_args = {.password_file = password_file,
                                    .total_password_count = total_password_count};
@@ -71,6 +150,7 @@ int main(int argc, char *const argv[]) {
     g_main_loop_unref(loop);
 
     lock_file_close(password_file);
+    free((void *)uri);
 
     return EXIT_SUCCESS;
 }
@@ -97,18 +177,18 @@ void channel_new_cb(SpiceSession *_session, SpiceChannel *channel, gpointer data
     g_object_get(channel, "channel-type", &channel_type, NULL);
 
     switch (channel_type) {
-    case SPICE_CHANNEL_MAIN:
-        g_signal_connect(channel, "channel-event", G_CALLBACK(channel_event_cb), data);
-        break;
-    case SPICE_CHANNEL_INPUTS:
-    case SPICE_CHANNEL_PLAYBACK:
-    case SPICE_CHANNEL_CURSOR:
-    case SPICE_CHANNEL_DISPLAY:
-    case SPICE_CHANNEL_USBREDIR:
-    case SPICE_CHANNEL_RECORD:
-        break;
-    default:
-        break;
+        case SPICE_CHANNEL_MAIN:
+            g_signal_connect(channel, "channel-event", G_CALLBACK(channel_event_cb), data);
+            break;
+        case SPICE_CHANNEL_INPUTS:
+        case SPICE_CHANNEL_PLAYBACK:
+        case SPICE_CHANNEL_CURSOR:
+        case SPICE_CHANNEL_DISPLAY:
+        case SPICE_CHANNEL_USBREDIR:
+        case SPICE_CHANNEL_RECORD:
+            break;
+        default:
+            break;
     }
 }
 
@@ -119,22 +199,22 @@ void channel_event_cb(SpiceChannel *channel, SpiceChannelEvent event, gpointer d
     g_object_get(channel, "spice-session", &session, NULL);
 
     switch (event) {
-    case SPICE_CHANNEL_ERROR_AUTH:
-        try_next_password(session, args);
-        break;
-    case SPICE_CHANNEL_OPENED: {
-        const char *password = NULL;
-        g_object_get(session, "password", &password, NULL);
-        printf("Password: %s\n", password);
-        spice_session_disconnect(session);
-        g_main_loop_quit(loop);
-        break;
-    }
-    default:
-        printf("%s: event %s\n", channel_to_string(channel), channel_event_to_string(event));
-        spice_session_disconnect(session);
-        g_main_loop_quit(loop);
-        break;
+        case SPICE_CHANNEL_ERROR_AUTH:
+            try_next_password(session, args);
+            break;
+        case SPICE_CHANNEL_OPENED: {
+            const char *password = NULL;
+            g_object_get(session, "password", &password, NULL);
+            printf("Password: %s\n", password);
+            spice_session_disconnect(session);
+            g_main_loop_quit(loop);
+            break;
+        }
+        default:
+            printf("%s: event %s\n", channel_to_string(channel), channel_event_to_string(event));
+            spice_session_disconnect(session);
+            g_main_loop_quit(loop);
+            break;
     }
 }
 
@@ -179,4 +259,20 @@ size_t count_passwords_in_file(lock_file_t *lf) {
 
     CHK_NEG(fseek(f, 0, SEEK_SET));
     return password_count;
+}
+
+void usage() {
+    printf(
+        "Usages:\n"
+        "  %1$s -h\n"
+        "  %1$s -w WORDLIST -u URI [-t THREADS]\n"
+        "  %1$s -w WORDLIST -H HOSTNAME [-p PORT] [-t THREADS]\n"
+        "Options:\n"
+        "  -h, --help    : displays this message and exits.\n"
+        "  -w, --wordlist: file containing passwords to try (separated by newlines).\n"
+        "  -u, --uri     : spice URI to use (spice://$HOSTNAME or spice://$HOSTNAME?port=$PORT).\n"
+        "  -H, --hostname: spice host to connect to.\n"
+        "  -p, --port    : spice port to connect to.\n"
+        "  -t, --threads : number of threads to use (default is %2$lu).\n",
+        program_invocation_short_name, THREADS_COUNT);
 }
